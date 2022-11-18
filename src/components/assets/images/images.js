80ce9569553691bdabca6a62d33cc909d6d72aa0
suraj-singh12 <surajsingh5092@gmail.com>
2022-11-19 03:22:00 +0530

    added assets

---
+++ src/components/assets/images/images.js
@@ -0,0 +1,12 @@
+const images = {
+    "card1": require('./Card 1.jpg'),
+    "card2": require('./Card 2.wepg'),
+    "card3": require('./Card 3.wepg'),
+    "card4": require('./Card 4.png'),
+    "ethereumIcon": require('./Etherium Icon.png'),
+    "hashDefine": require('./HashDefine_Background.jpg'),
+    "menuIcon": require('./Menu Icon.png'),
+    "userIcon": require('./User Icon.png')
+};
+
+export default images;
